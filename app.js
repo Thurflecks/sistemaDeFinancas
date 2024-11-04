42f6fd02f82bffb6d4e71fbddc6e6da0ea193b79
Thurflecks <thurflecks.dev@gmail.com>
2024-11-04 14:57:21 -0300

    configuraçoes iniciais

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const { engine } = require("express-handlebars")
+const app = express()
+const port = 8000
+const path = require("path")
+const router = require("./routers/Routers")
+const session = require("express-session")
+
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.engine("handlebars", engine({
+    defaultLayout: "main",
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+    }
+}))
+
+app.set("view engine", "handlebars")
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(router)
+
+app.use(session({
+    secret: 'senhaDoSistemaDeFinanças',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
+app.listen(port, () => {
+    console.log("site online no ar: http://localhost:8081")
+})
